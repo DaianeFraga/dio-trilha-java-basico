577b75be7cfeb2ac28b81472674622b1ec70bfa0
Daiane Fraga <146137781+DaianeFraga@users.noreply.github.com>
2023-09-28 21:13:01 -0300

    adicionei arquivos via upload

---
+++ estudos/src/estudos/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package estudos;
+
+/**
+ * @author Daiane
+ *
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+
+}
